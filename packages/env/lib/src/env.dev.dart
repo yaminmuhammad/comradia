d3062c7a61cc69bc7f7571421236ef9c1c600f0f
yaminmuhammad <muhammadyamin2801@gmail.com>
2025-02-06 22:39:44 +0700

    comment

--- packages/env/lib/src/env.dev.dart
+++ packages/env/lib/src/env.dev.dart
@@ -7,15 +7,15 @@ part 'env.dev.g.dart';
 /// {@endtemplate}
 @Envied(path: '.env.dev', obfuscate: true)
 abstract class EnvDev {
-  /// Supabase url secret.
+  /// Supabase url secret env.
   @EnviedField(varName: 'SUPABASE_URL', obfuscate: true)
   static String supabaseUrl = _EnvDev.supabaseUrl;
 
-  /// Supabase anon key secret.
+  /// Supabase anon key secret env.
   @EnviedField(varName: 'SUPABASE_ANON_KEY', obfuscate: true)
   static String supabaseKey = _EnvDev.supabaseKey;
 
-  /// PowerSync url secret.
+  /// PowerSync url secret env.
   @EnviedField(varName: 'POWERSYNC_URL', obfuscate: true)
   static String powersyncUrl = _EnvDev.powersyncUrl;
 }
