aee1cea0feb96f84d8a5d1fc6e639e5edaa1439e
yaminmuhammad <muhammadyamin2801@gmail.com>
2025-02-27 20:29:52 +0700

    chore: add initial configuration and issue templates for repository setup 12848

---
+++ packages/powersync_repository/test/src/powersync_repository_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:powersync_repository/powersync_repository.dart';
+
+void main() {
+  group('PowersyncRepository', () {
+    test('can be instantiated', () {
+      // expect(PowersyncRepository(), isNotNull);
+    });
+  });
+}
