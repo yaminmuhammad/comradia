bba652499dafaac5870f127105b411d66c7c6c87
yaminmuhammad <muhammadyamin2801@gmail.com>
2025-02-27 14:36:26 +0700

    chore: add example environment files and initial shared library structure

--- packages/shared/test/src/shared_test.dart
+++ packages/shared/test/src/shared_test.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: prefer_const_constructors
-// import 'package:shared/shared.dart';
+import 'package:shared/shared.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Shared', () {
     test('can be instantiated', () {
-      // expect(Shared(), isNotNull);
+      expect(Shared(), isNotNull);
     });
   });
 }
--- shared/lib/src/shared.dart
+++
@@ -1,7 +0,0 @@
-/// {@template shared}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class Shared {
-  /// {@macro shared}
-  const Shared();
-}
